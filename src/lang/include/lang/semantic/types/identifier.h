b34de64ca215933e772d7ac59d1044538edd1383
AlexBor78 <you@example.com>
2026-06-14 13:02:15 +0300

    documented memory idea

--- src/lang/include/lang/semantic/types/identifier.h
+++ src/lang/include/lang/semantic/types/identifier.h
@@ -1,127 +1,130 @@
- #pragma once
- 
- #include <climits>
- #include <cstddef>
- #include <lang/ast/ast.h>
- #include <lang/common/symbol_path.h>
- 
- namespace lang::semantic
- {
-     struct SymbolID
-     {
-         size_t id{ULLONG_MAX};
- 
-         bool operator==(const SymbolID& ohter) const noexcept {
-             return id == ohter.id;
-         }
-     };
-     struct Symbol
-     {
-         /**
-          * @brief   for error checking
-          */
-         bool is_valid{true};
- 
-         /**
-          * @brief unique id of symbol
-          */
-         SymbolID id;
- 
-         /**
-          * @brief   kind of symbol
-          */
-         enum class Kind {
-             VARIABLE,
-             FUNCTOIN,
-             MODULE,
-             TYPE,
-             NOT_VALID 
-         };
- 
-         /**
-          * @brief   kind of symbol: var, func, module or types
-          */
-         Kind kind{Symbol::Kind::NOT_VALID};
- 
-         /**
-          * @brief   pointer to node where it were declared
-          * @note    for modules - nullptr
-          */
-         ast::DeclStmt* decl{nullptr};
- 
-         /**
-          * @brief   SymbolPath to identifier
-          */
-         SymbolPath sympath;
- 
-         bool operator==(const Symbol& other) {
-             if(kind != other.kind) return false;
-             if(kind != Kind::MODULE && decl == other.decl) return true;
-             return sympath == other.sympath;
-         }
-     };
- }
- 
- namespace std {
-     /**
-      * @todo:   optimize by cache of hash
-      */
-     template<>
-     struct hash<lang::semantic::SymbolID> {
-         size_t operator()(const lang::semantic::SymbolID& id) const {
-             return hash<size_t>{}(id.id);
-         }
-     };
- 
-     // /**
-     //  * @todo:   optimize by cache of hash
-     //  */
-     // template<>
-     // struct hash<lang::semantic::Symbol> {
-     //     size_t operator()(const lang::semantic::Symbol& symbol) const {
-     //         return hash<lang::semantic::SymbolID>{}(symbol.id);
-     //     }
-     // };
- }
- 
- /**
-  * @brief   saved old code down
-  * @todo    remove old code
-  */
- namespace lang::semantic
- {
-     // class Scope;
-     // struct Identifier {
-     // public: 
-     //     enum class Kind {
-     //         VARIABLE,
-     //         FUNCTOIN,
-     //         MODULE,
-     //         TYPE
-     //     };
-     // public:
- 
-     //     /**
-     //      * @brief kind of identifier: var, func, module or types
-     //      */
-     //     Kind kind;
-         
-     //     /**
-     //      * @brief pointer to node where it were declared, for modules - nullptr
-     //      */
-     //     ast::DeclStmt* decl{nullptr};
- 
-     //     /**
-     //      * @brief absolute path to identifier (from global scope)
-     //      */
-     //     SymbolPath name;
- 
-     //     Identifier(SymbolPath _name
-     //     ,          Kind _kind
-     //     ,          ast::DeclStmt* _decl = nullptr
-     //     ):  name(std::move(_name))
-     //     ,   kind(_kind)
-     //     ,   decl(_decl)
-     //     {}
-     // };
- }
+#pragma once
+
+#include <climits>
+#include <cstddef>
+#include <variant>
+#include <lang/common/symbol_path.h>
+#include <lang/syntax/stmt.h>
+
+namespace lang::semantic {
+	 	class Symbol;
+  	class SymbolID {
+		private:
+				bool global{false};
+        union {
+				  size_t id{ULLONG_MAX};
+				  Symbol* symptr;
+				};
+ 		public:
+        inline bool operator==(const SymbolID& ohter) const noexcept {
+            return id == ohter.id;
+        }
+				inline bool is_global() const noexcept {return global;}
+				inline size_t get_global_id() const noexcept {return id;}
+				inline Symbol* get_local_ptr() noexcept {return symptr;}
+				inline const Symbol* get_local_ptr() const noexcept {return symptr;}
+    };
+}
+
+namespace std {
+    template<>
+    struct hash<lang::semantic::SymbolID> {
+        inline size_t operator()(const lang::semantic::SymbolID& id) const {
+            return hash<size_t>{}(id.get_global_id());
+        }
+    };
+}
+
+namespace lang::semantic {
+
+		struct TypeSymbol {
+		};
+
+		struct VarSymbol {
+				syntax::DeclVariable* decl;
+				SymbolID type;
+		};
+
+		struct FuncSymbol {
+				syntax::DeclFunction* decl;
+				SymbolID return_type;
+				SymbolID type;
+		};
+
+		struct ModuleSymbol {
+
+		};
+
+    class Symbol {
+		private:
+				std::variant<
+					TypeSymbol
+				,	VarSymbol
+				,	FuncSymbol
+				,	ModuleSymbol
+				> symbol;
+
+		public:
+        SymbolID id;
+        SymbolPath sympath;
+
+		public:
+				explicit Symbol(VarSymbol v) : symbol(std::move(v)) {}
+				explicit Symbol(TypeSymbol t) : symbol(std::move(t)) {}
+        explicit Symbol(FuncSymbol f) : symbol(std::move(f)) {}
+        explicit Symbol(ModuleSymbol m) : symbol(std::move(m)) {}
+
+				template <class T>
+				inline const T* as() const noexcept {
+            return std::get_if<T>(&symbol);
+        }
+
+				template <class T>
+				inline T* as() noexcept {
+            return std::get_if<T>(&symbol);
+        }
+    };
+}
+
+
+/**
+ * @brief   saved old code down
+ * @todo    remove old code
+ */
+namespace lang::semantic
+{
+    // class Scope;
+    // struct Identifier {
+    // public: 
+    //     enum class Kind {
+    //         VARIABLE,
+    //         FUNCTOIN,
+    //         MODULE,
+    //         TYPE
+    //     };
+    // public:
+
+    //     /**
+    //      * @brief kind of identifier: var, func, module or types
+    //      */
+    //     Kind kind;
+        
+    //     /**
+    //      * @brief pointer to node where it were declared, for modules - nullptr
+    //      */
+    //     ast::DeclStmt* decl{nullptr};
+
+    //     /**
+    //      * @brief absolute path to identifier (from global scope)
+    //      */
+    //     SymbolPath name;
+
+    //     Identifier(SymbolPath _name
+    //     ,          Kind _kind
+    //     ,          ast::DeclStmt* _decl = nullptr
+    //     ):  name(std::move(_name))
+    //     ,   kind(_kind)
+    //     ,   decl(_decl)
+    //     {}
+    // };
+}
